1a2e544f017e2d5c68b5f8650f1eae3500f5f636
Aditya-Raj143 <adityaxraj72004@gmail.com>
2024-11-24 16:52:53 +0530

    minor code fixes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,19 +3,27 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Auth } from "./pages/auth";
 import { Dashboard } from "./pages/dashboard";
 import { FinancialRecordsProvider } from "./context/financial-record-context";
+import { useUser } from "@clerk/clerk-react";
 
 function App() {
+  const { isSignedIn } = useUser();
+  console.log(isSignedIn);
+  
+
   return (
     <Router>
-      {""}
       <div className="app-container">
         <Routes>
           <Route
             path="/"
             element={
-              <FinancialRecordsProvider>
-                <Dashboard />
-              </FinancialRecordsProvider>
+              isSignedIn ? (
+                <FinancialRecordsProvider>
+                  <Dashboard />
+                </FinancialRecordsProvider>
+              ) : (
+                <Auth />
+              )
             }
           />
           <Route path="/auth" element={<Auth />} />
